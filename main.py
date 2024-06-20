1a7b5fb6128a45395bc976c56e9bbcf5c32aa95d
Shashwat Singh <shashwats17@gmail.com>
2024-06-20 16:46:48 -0400

    dict

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-students = {
-    "Herminoe": "Griffindor", 
-    "Harry": "Griffindor",
-    "Draco": "Slytherin"
-    }
+students = [
+    {"name" : "Hermione" , "house": "gryffindor", "patronus": "Otter"},
+    {"name" : "Harry" , "house": "gryffindor", "patronus": "Stag"},
+    {"name" : "Ron" , "house": "gryffindor", "patronus": "Jack Russell Terrier"},
+    {"name" : "Draco" , "house": "slytherin", "patronus": None}
 
+]
 
 for student in students:
-    print(student, students[student])
+    print(student["name"] , "," , student["house"] , "," ,  student["patronus"])
