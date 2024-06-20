856dc67b8d693b872b5825c4a633f8d7bf821eac
Shashwat Singh <shashwats17@gmail.com>
2024-06-20 16:06:42 -0400

    lists

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-def main():
-    meow(getnum())
+students = ["Hermmione" , "Harry", "Ron"]
 
-
-def getnum():
-    while True:
-        n = int(input("Give an int: "))
-        if n >= 0:
-            return n
-
-def meow(n):
-    for _ in range (n):
-        print ("Meow")
-
-main()
+for kid in students:
+    print (kid)
